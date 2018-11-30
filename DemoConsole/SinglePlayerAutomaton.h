eb84d443194d3b09a04bf1d84c00999e9e5c8c42
Bourgeois-Paquin Joe <0959600@decinfo.cvm>
2018-11-30 13:39:03 -0500

    mini push

--- DemoConsole/SinglePlayerAutomaton.h
+++ DemoConsole/SinglePlayerAutomaton.h
@@ -18,7 +18,7 @@ public:
 private:
 	State nextState(State state);
 	void startSinglePlayerAutomation(State state = State::Idle);
-	State update(State state);
+	//State update(State state);
 //	ElapsedTimer<> mTimer;
 	size_t slow_m;
 };
